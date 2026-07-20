74e6e4b19970abe943b101d30167e3ca1307011c
Clarissa Kristanto <clarissaakristanto@gmail.com>
2026-07-20 15:00:13 -0400

    Initial Commit

---
+++ FitIn/FitInApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitInApp.swift
+//  FitIn
+//
+//  Created by Clarissa Kristanto on 7/20/26.
+//
+
+import SwiftUI
+
+@main
+struct FitInApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
